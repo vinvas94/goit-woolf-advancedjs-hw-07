66ebca0c56686e7051aa7f4602549ba2674ea086
vinvas94 <vintoniv1111@gmail.com>
2023-12-07 22:55:50 +0100

    add todo

--- src/2.ts
+++ src/2.ts
@@ -16,15 +16,12 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -32,8 +29,10 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 1000);
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
-
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+// Клас Key
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+// Клас Person
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+// Абстрактний клас House
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person came in.");
+    } else {
+      console.log("Door is closed. Person cannot come in.");
+    }
+  }
+}
+
+// Клас MyHouse успадковується від абстрактного класу House
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door opened.");
+    } else {
+      console.log("Invalid key. Door remains closed.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +66,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
